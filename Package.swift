f338f0888a57848f7f21d8889617079dfe623d20
Gelareh Taban <gtaban@us.ibm.com>
2017-11-29 12:35:50 -0600

    update to swift 4

--- Package.swift
+++ Package.swift
@@ -1,19 +1,28 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 
 #if os(Linux)
-//    .Package(url: "https://github.com/gtaban/OpenSSL.git", majorVersion: 0)
-var dependencies: [Package.Dependency] = [
-    .Package(url: "https://github.com/IBM-Swift/OpenSSL.git", majorVersion: 0)
-]
+    var dependencies: [Package.Dependency] = [
+        .package(url: "https://github.com/IBM-Swift/OpenSSL.git", from: "0.0.0")
+    ]
 #else
     var dependencies: [Package.Dependency] = [
-    .Package(url: "https://github.com/IBM-Swift/OpenSSL-OSX.git", majorVersion: 0)
-]
+        .package(url: "https://github.com/IBM-Swift/OpenSSL-OSX.git", from: "0.0.0")
+    ]
 #endif
 
 let package = Package(
     name: "simpleRSA",
-    dependencies: dependencies
-
+    dependencies: dependencies,
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "simpleRSA",
+            dependencies: []
+        ),
+    ]
 )
