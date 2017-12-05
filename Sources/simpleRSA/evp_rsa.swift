7d94f4a9975d607790d6d3cf799e23a59133b022
Gelareh Taban <gtaban@us.ibm.com>
2017-12-05 17:18:42 -0600

    Add RSA envelope encrypt/decrypt. Separate out AES envelope stuff.

--- Sources/simpleRSA/evp_rsa.swift
+++ Sources/simpleRSA/evp_rsa.swift
@@ -8,37 +8,48 @@
 import Foundation
 import OpenSSL
 
+
 extension myRSA {
     
-    static let AES_KEYLEN_Bytes = 16 //256 bits
-    
     // EVP_PKEY = general private key without reference to any particular algorithm
-    static var localKeypair: UnsafeMutablePointer<EVP_PKEY>? = nil
-
-
-    // unsigned char *aesKey, *aesIV
-    static var aesKey: UnsafeMutablePointer<CUnsignedChar>? = nil
-    static var aesIV: UnsafeMutablePointer<CUnsignedChar>? = nil
+    static var rsaKeypair: UnsafeMutablePointer<EVP_PKEY>? = nil
 
     static var rsaEncryptCtx: UnsafeMutablePointer<EVP_CIPHER_CTX>? = nil
-    static var aesEncryptCtx: UnsafeMutablePointer<EVP_CIPHER_CTX>? = nil
     static var rsaDecryptCtx: UnsafeMutablePointer<EVP_CIPHER_CTX>? = nil
-    static var aesDecryptCtx: UnsafeMutablePointer<EVP_CIPHER_CTX>? = nil
 
-    public func evpRSADeInit() {
+    typealias PtrToPtr = UnsafeMutablePointer<UInt8>?
+
+    public func generateRSAKey() -> Bool {
+        
+        // Init RSA key
+        let ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, nil);
+        
+        if (EVP_PKEY_keygen_init(ctx) <= 0 ) {
+            print("FAILURE at EVP_PKEY_keygen_init")
+            return false
+        }
+        
+        // We really want to use EVP_PKEY_CTX_set_rsa_keygen_bits(ctx, RSA_KEY_LENGTH) but
+        // symbol isn't found so we are going to use what it defines
+        if (EVP_PKEY_CTX_ctrl(ctx, EVP_PKEY_RSA, EVP_PKEY_OP_KEYGEN, EVP_PKEY_CTRL_RSA_KEYGEN_BITS, RSA_KEY_LENGTH, nil) <= 0) {
+            print("FAILURE at EVP_PKEY_CTX_set_rsa_keygen_bits")
+            return false
+        }
+        
+        if(EVP_PKEY_keygen(ctx, &myRSA.rsaKeypair) <= 0) {
+            print("FAILURE at EVP_PKEY_keygen")
+            return false
+        }
+        EVP_PKEY_CTX_free(ctx);
+        return true
+    }
+    
+    public func evpRSADeinit() {
         
         let EVP_CIPHER_CTX_LENGTH = MemoryLayout<EVP_CIPHER_CTX>.size
 
         myRSA.rsaEncryptCtx?.deallocate(capacity: EVP_CIPHER_CTX_LENGTH)
-        myRSA.aesEncryptCtx?.deallocate(capacity: EVP_CIPHER_CTX_LENGTH)
         myRSA.rsaDecryptCtx?.deallocate(capacity: EVP_CIPHER_CTX_LENGTH)
-        myRSA.aesDecryptCtx?.deallocate(capacity: EVP_CIPHER_CTX_LENGTH)
-        
-        let AES_KEY_LENGTH = MemoryLayout<EVP_CIPHER_CTX>.size
-
-        myRSA.aesKey?.deallocate(capacity: AES_KEY_LENGTH)
-        myRSA.aesIV?.deallocate(capacity: AES_KEY_LENGTH)
-
     }
 
     // Creates the AES key and IV
@@ -49,204 +60,124 @@ extension myRSA {
         
         // rsaEncryptCtx = (EVP_CIPHER_CTX*)malloc(sizeof(EVP_CIPHER_CTX));
         myRSA.rsaEncryptCtx = UnsafeMutablePointer<EVP_CIPHER_CTX>.allocate(capacity: EVP_CIPHER_CTX_LENGTH)
-        myRSA.aesEncryptCtx = UnsafeMutablePointer<EVP_CIPHER_CTX>.allocate(capacity: EVP_CIPHER_CTX_LENGTH)
         myRSA.rsaDecryptCtx = UnsafeMutablePointer<EVP_CIPHER_CTX>.allocate(capacity: EVP_CIPHER_CTX_LENGTH)
-        myRSA.aesDecryptCtx = UnsafeMutablePointer<EVP_CIPHER_CTX>.allocate(capacity: EVP_CIPHER_CTX_LENGTH)
         
         EVP_CIPHER_CTX_init(myRSA.rsaEncryptCtx);
-        EVP_CIPHER_CTX_init(myRSA.aesEncryptCtx);
-
         EVP_CIPHER_CTX_init(myRSA.rsaDecryptCtx);
-        EVP_CIPHER_CTX_init(myRSA.aesDecryptCtx);
-
-        // Init RSA key
-        var ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, nil);
-
-        if (EVP_PKEY_keygen_init(ctx) <= 0 ) {
-            print("FAILURE at EVP_PKEY_keygen_init")
-            return false
-        }
-
-        // We really want to use EVP_PKEY_CTX_set_rsa_keygen_bits(ctx, RSA_KEY_LENGTH) but
-        // symbol isn't found so we are going to use what it defines
-        if (EVP_PKEY_CTX_ctrl(ctx, EVP_PKEY_RSA, EVP_PKEY_OP_KEYGEN, EVP_PKEY_CTRL_RSA_KEYGEN_BITS, RSA_KEY_LENGTH, nil) <= 0) {
-            print("FAILURE at EVP_PKEY_CTX_set_rsa_keygen_bits")
-            return false
-        }
         
-
-        if(EVP_PKEY_keygen(ctx, &myRSA.localKeypair) <= 0) {
-            print("FAILURE at EVP_PKEY_keygen")
-            return false
-        }
-        EVP_PKEY_CTX_free(ctx);
-
-        // Init AES
-        myRSA.aesKey = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: myRSA.AES_KEYLEN_Bytes)
-        myRSA.aesIV = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: myRSA.AES_KEYLEN_Bytes)
-        
-        let aesPass = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: myRSA.AES_KEYLEN_Bytes)
-        
-        // we use salt if we want to use PBKDF for AES key generation
-//        let aesSalt = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: 8)
-
-        defer {
-            aesPass.deallocate(capacity: myRSA.AES_KEYLEN_Bytes)
-//            aesSalt.deallocate(capacity: 8)
-        }
-        
-        // AES key generation - just do random
-        
-        // For the AES key we have the option of using a PBKDF (password-baswed key derivation formula)
-        // or just using straight random data for the key and IV. Depending on your use case, you will
-        // want to pick one or another.
-//        #ifdef USE_PBKDF
-//        // Get some random data to use as the AES pass and salt
-//        if(RAND_bytes(aesPass, AES_KEYLEN/8) == 0) {
-//            return FAILURE;
-//        }
-//
-//        if(RAND_bytes(aesSalt, 8) == 0) {
-//            return FAILURE;
-//        }
-//
-//        if(EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha256(), aesSalt, aesPass, AES_KEYLEN/8, AES_ROUNDS, aesKey, aesIV) == 0) {
-//            return FAILURE;
-//}
-//#else
-
-        if(RAND_bytes(myRSA.aesKey, Int32(myRSA.AES_KEYLEN_Bytes)) == 0) {
-            print("FAILURE at RAND_bytes for aesKey")
-            return false
-        }
-        
-        if(RAND_bytes(myRSA.aesIV, Int32(myRSA.AES_KEYLEN_Bytes)) == 0) {
-            print("FAILURE at RAND_bytes for aesIV")
-            return false
-        }
-
-//#endif // USE_PBKDF
-
         return true
     }
     
-    //        int Crypto::aesEncrypt(const unsigned char *msg, size_t msgLen, unsigned char **encMsg)
-    public func evpAESEncrypt(plaintext: String,
-                              ciphertext: inout UnsafeMutablePointer<UInt8>? ,
-                              cipherLength: inout Int32) -> Bool {
-
-        // set up cipher context for encryption with cipher type EVP_aes_256_cbc()
-        // return 1 for success and 0 for failure.
-        var status = EVP_EncryptInit_ex(myRSA.aesEncryptCtx, EVP_aes_256_cbc(), nil, myRSA.aesKey, myRSA.aesIV)
-        if(status == 0) {
-            print("FAILURE at EVP_EncryptInit_ex")
-            return false
-        }
-        
-        print("Plaintext (\(plaintext.utf8.count)): \(plaintext)");
+    
+    //        rsaEncrypt(unsigned char **encMsg, unsigned char **ek, size_t *ekl, unsigned char **iv, size_t *ivl) {
+    public func rsaEncrypt(plaintext: String,
+                           ciphertext: inout UnsafeMutablePointer<UInt8>? ,
+                           cipherLength: inout Int32,
+                           encKey: inout UnsafeMutablePointer<UInt8>? ,
+                           encKeyLength: inout Int32,
+                           IV: inout UnsafeMutablePointer<UInt8>? ,
+                           IVLength: inout Int32 ) -> Bool {
         
         var encLength: Int32 = 0
-        let enc = UnsafeMutablePointer<UInt8>.allocate(capacity: plaintext.count + myRSA.AES_KEYLEN_Bytes - 1)
-
-        // ciphertext = out, outl where outl = [0 ... (inl + cipher_block_size - 1) ]
-        // return 1 for success and 0 for failure.
-        status = EVP_EncryptUpdate(myRSA.aesEncryptCtx, enc, &encLength, plaintext, Int32(plaintext.count))
+        var ek: PtrToPtr
+        ek = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(EVP_PKEY_size(myRSA.rsaKeypair)))
         
-        print(EVP_CIPHER_block_size(EVP_aes_256_cbc()))
+        // we need to create an array of pointers with ek
+        let ekPtr = UnsafeMutablePointer<PtrToPtr>.allocate(capacity: MemoryLayout<PtrToPtr>.size)
+        ekPtr.pointee = ek
+
+//        IVLength = EVP_CIPHER_CTX_iv_length(myRSA.rsaEncryptCtx) // EVP_MAX_IV_LENGTH
+        IVLength = EVP_MAX_IV_LENGTH
+        let iv = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(IVLength))
         
-        var encrypted_str = String(cString: UnsafePointer(enc))
-        print("Ciphertext (\(encLength))= \(encrypted_str)")
+        let enc = UnsafeMutablePointer<UInt8>.allocate(capacity: plaintext.count + Int(IVLength))
 
-        cipherLength = cipherLength + encLength;
-
-        // encrypts the "final" data, that is any data that remains in a partial block.
-        status = EVP_EncryptFinal_ex(myRSA.aesEncryptCtx, enc.advanced(by: Int(cipherLength)), &encLength)
-        if(status == 0) {
-            print("FAILURE at EVP_EncryptInit_ex")
+        // initializes a cipher context ctx for encryption with cipher type using a random secret key and IV.
+        // The secret key is encrypted using the public key (can be a set of public keys)
+        // ek size = EVP_PKEY_size(pubk[i]) bytes
+        // iv must contain enough room for the corresponding cipher's IV, as determined by (for example) EVP_CIPHER_iv_length(type).
+        var status = EVP_SealInit(myRSA.rsaEncryptCtx, EVP_aes_256_cbc(), ekPtr, &encKeyLength, iv, &myRSA.rsaKeypair, 1)
+        guard status != 0 else {
+            print("FAILURE at EVP_SealInit")
             return false
         }
-        cipherLength = cipherLength + encLength;
 
-        encrypted_str = String(cString: UnsafePointer(enc))
+        //  int EVP_SealUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl, unsigned char *in, int inl);
+        // We really want to use EVP_SealUpdate but symbols don't resolve. So based on evp.h:
+        // # define EVP_SealUpdate(a,b,c,d,e)       EVP_EncryptUpdate(a,b,c,d,e)
+        status = EVP_EncryptUpdate(myRSA.rsaEncryptCtx, enc, &encLength, plaintext, Int32(plaintext.count))
+        guard status != 0 else {
+            print("FAILURE at EVP_SealInit")
+            return false
+        }
+        cipherLength = encLength
+
+        status = EVP_SealFinal(myRSA.rsaEncryptCtx, enc.advanced(by: Int(cipherLength)), &encLength)
+        guard status != 0 else {
+            print("FAILURE at EVP_SealInit")
+            return false
+        }
+        cipherLength = cipherLength + encLength
+        
+        // assign values to be returned
+        ciphertext = enc
+        IV = iv
+        encKey = ek
+
+        let encrypted_str = String(cString: UnsafePointer(enc))
         print("Ciphertext (\(cipherLength))= \(encrypted_str)")
 
-        ciphertext = enc
-        EVP_CIPHER_CTX_cleanup(myRSA.aesEncryptCtx);
+        EVP_CIPHER_CTX_cleanup(myRSA.rsaEncryptCtx);
         
         return true
     }
     
-    public func evpAESDecrypt(ciphertext: UnsafeMutablePointer<UInt8> ,
+    //        int Crypto::rsaDecrypt(unsigned char *encMsg, size_t encMsgLen, unsigned char *ek, size_t ekl,
+    //        unsigned char *iv, size_t ivl, unsigned char **decMsg) {
+    public func rsaDecryption(ciphertext: UnsafeMutablePointer<UInt8> ,
                               cipherLength: Int32,
+                              encKey: UnsafeMutablePointer<UInt8> ,
+                              encKeyLength: Int32,
+                              IV: UnsafeMutablePointer<UInt8> ,
+                              IVLength: Int32,
                               decMsg: inout UnsafeMutablePointer<UInt8>? ,
-                              decMsgLen: inout Int32) -> UInt32 {
-
+                              decMsgLen: inout Int32) -> Bool {
+        
         var decLen: Int32 = 0
         var processedLen: Int32 = 0
-
-        let decrypted = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(cipherLength))
-
-        var status = EVP_DecryptInit_ex(myRSA.aesDecryptCtx, EVP_aes_256_cbc(), nil, myRSA.aesKey, myRSA.aesIV)
-        if (status == 0) {
-            print("FAILURE at EVP_DecryptInit_ex")
-            return 0
-        }
         
-        status = EVP_DecryptUpdate(myRSA.aesDecryptCtx, decrypted, &processedLen, ciphertext, cipherLength)
-        if (status == 0) {
+        let decrypted = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(cipherLength + IVLength))
+        
+        let key = myRSA.rsaKeypair
+        
+        var status = EVP_OpenInit(myRSA.rsaDecryptCtx, EVP_aes_256_cbc(), encKey, encKeyLength, IV, key)
+        guard status != 0 else {
+            print("FAILURE at EVP_OpenInit")
+            return false
+        }
+
+        // We really want to use EVP_OpenUpdate but symbols don't resolve. So based on evp.h:
+        // # define EVP_OpenUpdate(a,b,c,d,e)       EVP_DecryptUpdate(a,b,c,d,e)
+        status = EVP_DecryptUpdate(myRSA.rsaDecryptCtx, decrypted, &processedLen, ciphertext, cipherLength)
+        guard status != 0 else {
             print("FAILURE at EVP_DecryptUpdate")
-            return 0
+            return false
         }
-        decLen = decLen + processedLen
         
-        status = EVP_DecryptFinal_ex(myRSA.aesDecryptCtx, decrypted.advanced(by: Int(decLen)), &processedLen)
-        if (status == 0) {
-            print("FAILURE at EVP_DecryptFinal_ex")
-            return 0
+        decLen = processedLen;
+        
+        status = EVP_OpenFinal(myRSA.rsaDecryptCtx, decrypted.advanced(by: Int(decLen)), &processedLen)
+        guard status != 0 else {
+            print("FAILURE at EVP_OpenFinal")
+            return false
         }
         decLen = decLen + processedLen
 
-        EVP_CIPHER_CTX_cleanup(myRSA.aesDecryptCtx);
-        
         let encrypted_str = String(cString: UnsafePointer(decrypted))
         print("DECRYPTED (\(decLen))= \(encrypted_str)")
 
-        return 1
-    }
-    
-    //        rsaEncrypt(const unsigned char *msg, size_t msgLen, unsigned char **encMsg, unsigned char **ek, size_t *ekl, unsigned char **iv, size_t *ivl) {
-    public func rsaEncrypt() -> Bool {
-        
-//            size_t encMsgLen = 0;
-//            size_t blockLen  = 0;
-//
-//            *ek = (unsigned char*)malloc(EVP_PKEY_size(remotePubKey));
-//            *iv = (unsigned char*)malloc(EVP_MAX_IV_LENGTH);
-//            if(*ek == NULL || *iv == NULL) return FAILURE;
-//            *ivl = EVP_MAX_IV_LENGTH;
-//
-//            *encMsg = (unsigned char*)malloc(msgLen + EVP_MAX_IV_LENGTH);
-//            if(encMsg == NULL) return FAILURE;
-//
-//            if(!EVP_SealInit(rsaEncryptCtx, EVP_aes_256_cbc(), ek, (int*)ekl, *iv, &remotePubKey, 1)) {
-//                return FAILURE;
-//            }
-//
-//            if(!EVP_SealUpdate(rsaEncryptCtx, *encMsg + encMsgLen, (int*)&blockLen, (const unsigned char*)msg, (int)msgLen)) {
-//                return FAILURE;
-//            }
-//            encMsgLen += blockLen;
-//
-//            if(!EVP_SealFinal(rsaEncryptCtx, *encMsg + encMsgLen, (int*)&blockLen)) {
-//                return FAILURE;
-//            }
-//            encMsgLen += blockLen;
-//
-//            EVP_CIPHER_CTX_cleanup(rsaEncryptCtx);
-//
-//            return (int)encMsgLen;
-//        }
-        
+        EVP_CIPHER_CTX_cleanup(myRSA.rsaDecryptCtx);
         return true
     }
+    
 }
--- Sources/simpleRSA/main.swift
+++ Sources/simpleRSA/main.swift
@@ -1,12 +1,12 @@
 import Foundation
+import OpenSSL
 
 
-let v = myRSA()
-_ = v.evpRSAInit()
+let u = myAES()
 
+let plaintext = "In those days spirits were brave, the stakes were high, men were real men, women were real women and small furry creatures from Alpha Centauri were real small furry creatures from Alpha Centauri."
 
-let plaintext = "The Swift Programming Language (Swift 4)"
-
+let AES_KEYLEN_Bytes = 16 //256 bits
 
 //var ciphertext =  String()
 // AES ciphertext has range [0 ... (inl + cipher_block_size - 1) ]
@@ -25,18 +25,100 @@ defer {
 
 var decryptedtextLength: Int32 = 0
 
-_ = v.evpAESEncrypt(plaintext: plaintext, ciphertext: &ciphertextAES, cipherLength: &ciphertextLength)
+var status = u.generateAESKey(of: AES_KEYLEN_Bytes)
+guard status == true else {
+    print("ERROR: generateAESKey")
+    exit(0)
+}
+
+status = u.evpAESEncrypt(plaintext: plaintext, ciphertext: &ciphertextAES, cipherLength: &ciphertextLength)
+guard status == true else {
+    print("ERROR: evpAESEncrypt")
+    exit(0)
+}
 
 
 if let cipher = ciphertextAES {
     let encrypted_str = String(cString: UnsafePointer(cipher))
     print("Ciphertext (\(ciphertextLength))= \(encrypted_str)")
     
-    _ = v.evpAESDecrypt(ciphertext: cipher, cipherLength: ciphertextLength, decMsg: &decryptedtext, decMsgLen: &decryptedtextLength)
-
+    status = u.evpAESDecrypt(ciphertext: cipher, cipherLength: ciphertextLength, decMsg: &decryptedtext, decMsgLen: &decryptedtextLength)
+    guard status == true else {
+        print("ERROR: evpAESDecrypt")
+        exit(0)
+    }
 
 } else {
     print("NULL")
 }
+
+let v = myRSA()
+
+status = v.evpRSAInit()
+guard status == true else {
+    print("ERROR: evpRSAInit")
+    exit(0)
+}
+
+
+var ciphertextRSA: UnsafeMutablePointer<UInt8>? = nil
+defer {
+    ciphertextRSA?.deallocate(capacity: plaintext.count + 16 - 1)
+}
+var ciphertextRSALength: Int32 = 0
+
+var encKey: UnsafeMutablePointer<UInt8>? = nil
+defer {
+    encKey?.deallocate(capacity: plaintext.count + 16 - 1)
+}
+var encKeyLength: Int32 = 0
+
+var iv: UnsafeMutablePointer<UInt8>? = nil
+defer {
+    iv?.deallocate(capacity: Int(EVP_MAX_IV_LENGTH))
+}
+var ivLength: Int32 = 0
+
+
+var decryptedtextRSA: UnsafeMutablePointer<UInt8>? = nil
+defer {
+    decryptedtextRSA?.deallocate(capacity: plaintext.count + 16 - 1)
+}
+
+var decryptedtextRSALength: Int32 = 0
+
+status = v.generateRSAKey()
+guard status == true else {
+    print("ERROR: generateRSAKey")
+    exit(0)
+}
+
+status = v.rsaEncrypt(plaintext: plaintext, ciphertext: &ciphertextRSA, cipherLength: &ciphertextRSALength, encKey: &encKey, encKeyLength: &encKeyLength, IV: &iv, IVLength: &ivLength)
+
+if let cipher = ciphertextRSA, let ek = encKey, let iv = iv {
+    let encrypted_str = String(cString: UnsafePointer(cipher))
+    print("Ciphertext (\(ciphertextLength))= \(encrypted_str)")
+    
+    status = v.rsaDecryption(ciphertext: cipher, cipherLength: ciphertextRSALength, encKey: ek, encKeyLength: encKeyLength, IV: iv, IVLength: ivLength, decMsg: &decryptedtextRSA, decMsgLen: &decryptedtextRSALength)
+    guard status == true else {
+        print("ERROR: evpAESDecrypt")
+        exit(0)
+    }
+    
+} else {
+    print("NULL")
+}
+
+
+
+v.evpRSADeinit()
+
+
+
+
+
+
+
+
 
 
